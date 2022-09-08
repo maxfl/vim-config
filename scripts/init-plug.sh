5ed0cf97bc548b6cb33b961bab9a4197622eca80
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2022-09-08 10:24:39 +0300

    misc

--- scripts/init-plug.sh
+++ scripts/init-plug.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+set -euo pipefail # strict mode
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim -c PlugInstall
